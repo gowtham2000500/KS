ab3875cec600388215ea11d092147e35f4def959
gowtham <rtgautham@gmail.com>
2023-02-18 14:12:12 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import ks from './ks.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ks} className="App-logo" alt="logo" />
+        <p>
+         KS Tavels Contact Details:9629272008
+         valliyamalpuram
+         
+        </p>
+        <a
+          className="App-link"
+          href="https://wa.me/c/9629272008"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact ME
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
